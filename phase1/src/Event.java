fd432253b828824c6312b6403ff71b4a32b236de
Archay <archay.dutta@mail.utoronto.ca>
2020-11-04 02:11:14 +0400

    Started Event.java

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,3 +1,32 @@
-public class Event{ // test
+import java.util.Date;
+public class Event{
+    private int eventId;
+    private String eventName;
+    private Speaker[] eventSpeaker;
+    private Date eventDate;
+    private String eventLocation;
+
+    /**
+     * Constructs a new Event object
+     * @param eventDate: A Date object
+     * @param eventLocation: Name of a location, is a String
+     * @param eventName: Name of event, is a String
+     * @param eventSpeaker: a List of Speakers
+     */
+    public Event(String eventName, Speaker[] eventSpeaker, Date eventDate, String eventLocation) {
+        this.eventName = eventName;
+        this.eventSpeaker = eventSpeaker;
+        this.eventDate = eventDate;
+        this.eventLocation = eventLocation;
+        this.generateID();
+    }
+
+    /**
+     * Generates an ID for event.
+     * TODO: 11/4/2020: Decide ID generation
+     */
+    public void generateID() {
+
+    }
 
 }
