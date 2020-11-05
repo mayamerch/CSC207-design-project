2dbde7e6cc70f4b4659a3c4a649380992b57a7f4
Maya <maya.merchant@mail.utoronto.ca>
2020-11-05 13:47:29 -0500

    test

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -3,18 +3,18 @@ import java.util.ArrayList;
 public class Message { // storing all messages
     public String content;
     public String from;
-    public ArrayList<String> recipients; // for a single message, length 1
+    public ArrayList<String> to; // for a single message, length 1
 
     /**
      * Constructs a new Message object
      * @param content: content of the message (String)
      * @param from: name of the sender of the message (String)
-     * @param recipients: name of the receiver of the message (ArrayList)
+     * @param to: name of the receiver of the message (ArrayList)
      */
-    public Message(String content, String from, ArrayList<String> recipients){
+    public Message(String content, String from, ArrayList<String> to){
         this.content = content;
         this.from = from;
-        this.recipients = recipients;
+        this.to = to;
     }
 
     public void setContent(String c){
@@ -23,8 +23,8 @@ public class Message { // storing all messages
     public void setFrom(String f){
         this.from = f;
     }
-    public void setTo(ArrayList<String> r){
-        this.recipients = r;
+    public void setTo(ArrayList<String> t){
+        this.to = t;
     }
 
     public String getContent(){
@@ -34,7 +34,7 @@ public class Message { // storing all messages
         return from;
     }
     public ArrayList<String> getTo() {
-        return recipients;
+        return to;
     }
 
 }
