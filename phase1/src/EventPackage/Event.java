2531ee80587e75d86c6e77b4651f1e0d68ff4392
Adam Musa <adamm.musa@mail.utoronto.ca>
2020-11-05 15:58:41 -0330

    Added getters for instance variables

--- phase1/src/EventPackage/Event.java
+++ phase1/src/EventPackage/Event.java
@@ -6,21 +6,21 @@ import java.util.List;
 public class Event{
     private static int idCounter;
     private int eventId;
-    private List<Integer> eventAttendees;
     private String eventName;
-    private Integer eventSpeaker;
     private Date eventDate;
-    private String eventLocation;
+    private Integer eventLocation;
+    private Integer eventSpeaker;
+    private List<Integer> eventAttendees;
 
     /**
      * Constructs a new EventPackage.Event object
      * @param eventDate: A Date object
-     * @param eventLocation: Name of a location, is a String
+     * @param eventLocation: Number of Room the event is in.
      * @param eventName: Name of event, is a String
      * @param eventSpeaker: The ID of a speaker at an event
      * @param eventAttendees: A list of IDs of Attendees of the event
      */
-    public Event(String eventName, Integer eventSpeaker, Date eventDate, String eventLocation,
+    public Event(String eventName, Integer eventSpeaker, Date eventDate, Integer eventLocation,
                  List<Integer> eventAttendees) {
         this.eventName = eventName;
         this.eventSpeaker = eventSpeaker;
@@ -33,12 +33,69 @@ public class Event{
 
 
     /**
-     * Takes in a List of IDs of Attendees that are going to attend the event and adds them to the
-     * List of IDs of Attendees already attending the event.
-     * @param AttendeeList: List of IDs of Attendees we need to add.
+     * Returns the ID of the current event
+     * @return      The ID of this Event
      */
-    public void AddAttendees(List<Integer> AttendeeList) {
-        eventAttendees.addAll(AttendeeList);
+    public int getEventId() {
+        return eventId;
     }
+
+
+    /**
+     * Returns the name of the current event
+     * @return      The name of this Event
+     */
+    public String getEventName() {
+        return eventName;
+    }
+
+
+    /**
+     * Returns the date of the current event
+     * @return      The date of this Event
+     */
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+
+    /**
+     * Returns the number of the room the current event is held
+     * @return      The number of the room the event is held in
+     */
+    public Integer getEventLocation() {
+        return eventLocation;
+    }
+
+
+    /**
+     * Returns the List of the IDs of the Attendees attending the event
+     * @return      List of IDs of Attendees attending
+     */
+    public List<Integer> getEventAttendees() {
+        return eventAttendees;
+    }
+
+
+    /**
+     * Returns the ID of the Speaker at this event
+     * @return      The ID of the Speaker at this event
+     */
+    public Integer getEventSpeaker() {
+        return eventSpeaker;
+    }
+
+
+    /**
+     * Adds an Attendee to an event by adding its ID to the list Attendees attending the event
+     * @param AttendeeID: List of IDs of Attendees we need to add.
+     */
+    public void AddAttendee(Integer AttendeeID) {
+        eventAttendees.add(AttendeeID);
+    }
+
+
+
+
 
 }
