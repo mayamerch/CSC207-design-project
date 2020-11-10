e119e684b185e81280f9206107c5d804ff18e0c9
Maya <maya.merchant@mail.utoronto.ca>
2020-11-10 14:31:03 -0500

    test

--- phase1/src/MessagePackage/ConversationController.java
+++ phase1/src/MessagePackage/ConversationController.java
@@ -1,15 +1,18 @@
 package MessagePackage;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ConversationController {
     public ArrayList<Conversation> conversations;
+    public Iterator iterator;
 
     /**
      * Creates an instance of ConversationSystem that contains all the recorded conversations (empty at first)
      */
-    public ConversationController(){
-        this.conversations = new ArrayList<Conversation>();
+    public ConversationController(ArrayList<Conversation> conversations){
+        this.conversations = conversations;
+        this.iterator = conversations.iterator();
     }
 
     public void createConversation(){
@@ -26,6 +29,12 @@ public class ConversationController {
 
     public ArrayList<Conversation> returnConversationsforUserID(){
         return null;
+    }
+
+    public void iterate(){
+        while(iterator.hasNext()){
+            iterator.next();
+        }
     }
 
 }
