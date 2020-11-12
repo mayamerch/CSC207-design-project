86203fc4d08351f7c53f1cb1fa44331a2fa1006f
jzhang <jennxy.zhang@mail.utoronto.ca>
2020-11-12 13:16:53 -0500

    modified ConversationGateway methods

--- phase1/src/MessagePackage/ConversationGateway.java
+++ phase1/src/MessagePackage/ConversationGateway.java
@@ -1,10 +1,10 @@
 package MessagePackage;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import EventPackage.Event;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 public class ConversationGateway {
     private File chatroomDataFile;
@@ -17,22 +17,72 @@ public class ConversationGateway {
      */
     public ConversationGateway(){
         try {
-            File file = new File("phase1/src/MessagePackage/ConversationData.txt");
-
-
-
+            this.chatroomDataFile = new File("phase1/src/EventPackage/chatroomData.txt");
+            if (this.chatroomDataFile.createNewFile()) {
+                this.chatroomData = new ArrayList<>();
+            } else {
+                this.chatroomData = conversationsReader(chatroomDataFile);
+            }
         } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try {
+            this.broadcastDataFile = new File("phase1/src/EventPackage/broadcastData.txt");
+            if (this.broadcastDataFile.createNewFile()) {
+                this.broadcastData= new ArrayList<>();
+            } else {
+                this.broadcastData = conversationsReader(broadcastDataFile);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
 
-    public Conversation ConversationReader(){
+
+    /**
+     * Write the data for conversations currently in ConversationController to chatroomDataFile and broadcastDataFile
+     */
+    private void conversationsWriter(){
+        try {
+            FileWriter eventWriter = new FileWriter(this.chatroomDataFile, true);
+            for (Event e : eventList) {
+                eventWriter.write(e.toString() + System.lineSeparator());
+                eventWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     * Takes data from chatroomData.txt and broadcastData.txt and tries to write into chatroomData and broadcastData
+     * Prints error message to console if attempt failed.
+     */
+    private ArrayList<StringBuilder> conversationsReader(File file){
+
         return null;
     }
 
-    private ArrayList<StringBuilder> ConversationParser(){
+    /**
+     * @param readStrings an ArrayList of StringBuilder that has information on each conversation
+     * @return an ArrayList of Chatroom or Broadcast
+     */
 
+    public ArrayList<Conversation> conversationParser(ArrayList<StringBuilder> readStrings){
+        ArrayList<StringBuilder> readText = new ArrayList<>();
+        try {
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return data;
     }
-
 
 }
