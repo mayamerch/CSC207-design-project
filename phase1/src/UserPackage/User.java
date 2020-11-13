98dbf6ea8a6c0b81dc64194fd448105589891d23
Edward Leung <eds.leung@mail.utoronto.ca>
2020-11-13 15:38:09 -0500

    Removed undesired change.

--- phase1/src/UserPackage/User.java
+++ phase1/src/UserPackage/User.java
@@ -18,7 +18,6 @@ public abstract class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.conversations = new ArrayList<Conversation>();
     }
     // user_ID needs to be unique so it needs to be assigned to user by something above like a use case
     // which can access all the usernames. Since use cases can edit entity info they can assign
