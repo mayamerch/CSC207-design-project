33139c4c4299dbc6c81244c1489398f92ba4228d
jzhang <jennxy.zhang@mail.utoronto.ca>
2020-11-16 14:14:26 -0500

    ChatroomGateway and BroadcastGateway made

---
+++ phase1/src/MessagePackage/BroadcastGateway.java
@@ -0,0 +1,9 @@
+package MessagePackage;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class BroadcastGateway {
+    private File broadcastDataFile;
+    private ArrayList<StringBuilder> broadcastData;
+}
---
+++ phase1/src/MessagePackage/ChatroomGateway.java
@@ -0,0 +1,9 @@
+package MessagePackage;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ChatroomGateway {
+    private File chatroomDataFile;
+    private ArrayList<StringBuilder> chatroomData;
+}
