ed8a0cc8da7f8ef76b5e003bfc18e3f711ef860b
ritvikpuri <ritvikpuri00@gmail.com>
2020-11-19 13:01:35 -0500

    Updated UserPresenter

--- phase1/src/UserPackage/UserPresenter.java
+++ phase1/src/UserPackage/UserPresenter.java
@@ -6,13 +6,21 @@ public class UserPresenter {
 
     public void printFriendMenu(int userId){
         Scanner sc = new Scanner(System.in);
+        UserManager userManager = new UserManager();
+        User loggedIn = userManager.getUserByID(userId);
+
         System.out.println("What would you like to do?\n" +
                 "1. View Current Friends\n" +
                 "2. Friend Requests\n" +
                 "3. Add a new friend\n" +
-                "4. Add a new friend\n" +
                 "Please input a number: ");
         Integer i = sc.nextInt();
+
+        switch (i){
+            case 1: System.out.println(loggedIn.getFriendsList());
+            case 2: System.out.println(loggedIn.getFriendRequestList());
+            case 3: //Create Add Friend  ;
+        }
 
     }
 }
