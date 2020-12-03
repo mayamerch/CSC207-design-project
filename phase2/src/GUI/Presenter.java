f667df606357ac410a82df848b5080f0699a328c
Edward Leung <eds.leung@mail.utoronto.ca>
2020-12-03 14:18:24 -0500

    Added friends in code for presenter

--- phase2/src/GUI/Presenter.java
+++ phase2/src/GUI/Presenter.java
@@ -9,7 +9,31 @@ public class Presenter {
         UserManager userManager = new UserManager();
 
         userManager.createAccount("user1", "user1", UserType.ATTENDEE);
+        userManager.createAccount("user2", "user2", UserType.ATTENDEE);
+        userManager.createAccount("user3", "user3", UserType.ATTENDEE);
+        userManager.createAccount("user4", "user4", UserType.ATTENDEE);
+        userManager.createAccount("user5", "user5", UserType.ATTENDEE);
         UserController userController = new UserController(userManager);
+        userController.userLogin("user1", "user1");
+        userController.sendFriendRequest("user2");  // sent request to user2
+        userController.sendFriendRequest("user3"); //
+        userController.logOut();
+        userController.userLogin("user2", "user2");
+        userController.acceptFriendRequest("user1"); // accept user 1 from user2
+        userController.logOut();
+        userController.userLogin("user3", "user3");
+        userController.acceptFriendRequest("user1"); // accept user 1 from user3
+        userController.logOut();
+        userController.userLogin("user4", "user4");
+        userController.sendFriendRequest("user1");
+        userController.logOut();
+        userController.userLogin("user1", "user1"); // log in as user1
+        //new FriendMenuView(userController);
+        // making a bunch of users to test scroll on friend request
+        for (int i=6; i< 30; i++){
+            userManager.createAccount("user" + i, "user"+i, UserType.ATTENDEE);
+            userManager.sendFriendRequest(i, 1);
+        }
 
         LoginView loginView = new LoginView(userController);
         while (userController.validateNotLoggedIn()) {
