6edf454c1359f7aab424754e53db9b509dfa80fe
Edward Leung <eds.leung@mail.utoronto.ca>
2020-12-03 13:31:26 -0500

    Chanaged presenter test so you can log in

--- phase2/src/GUI/Presenter.java
+++ phase2/src/GUI/Presenter.java
@@ -1,12 +1,18 @@
 package GUI;
 import UserPackage.UserController;
+import UserPackage.UserManager;
+import UserPackage.UserType;
 
 public class Presenter {
 
     public static void main(String[] args) {
-        UserController userController = new UserController();
+        UserManager userManager = new UserManager();
+
+        userManager.createAccount("user1", "user1", UserType.ATTENDEE);
+        UserController userController = new UserController(userManager);
+
         LoginView loginView = new LoginView(userController);
-        while (!userController.validateNotLoggedIn()) {
+        while (userController.validateNotLoggedIn()) {
             loginView.setVisible(true);
         }
         loginView.setVisible(false);
