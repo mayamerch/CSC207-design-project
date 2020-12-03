2ddbc6cf5092d3d1863891a63bab896fce561785
Andy Chan <andykaysum.chan@mail.utoronto.ca>
2020-12-02 23:19:43 -0500

    - added Login GUI (work in progress) - added a constructor for UserController with UserManager as a parameter (for testing code mainly)

---
+++ phase2/src/GUI/LoginTest.java
@@ -0,0 +1,24 @@
+package GUI;
+
+import UserPackage.User;
+import UserPackage.UserController;
+import UserPackage.UserManager;
+import UserPackage.UserType;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class LoginTest {
+    public static void main(String[] args){
+
+        HashMap<Integer, User> userMap = new HashMap<>();
+        UserManager userManager = new UserManager();
+
+        userManager.createAccount("user1", "passy", UserType.ATTENDEE);
+        userManager.createAccount("user2", "letmeinpls", UserType.ORGANIZER);
+
+        UserController userController = new UserController(userManager);
+        JFrame frame = new Login(userController);
+        frame.setVisible(true);
+    }
+}
