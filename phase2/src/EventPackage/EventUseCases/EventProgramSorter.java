94760a30fcd02da2ec1913b36b4dba5db2a6447b
jzhang <jennxy.zhang@mail.utoronto.ca>
2020-12-05 03:29:59 -0500

    Completed basic required methods in EventProgramSorter

--- phase2/src/EventPackage/EventUseCases/EventProgramSorter.java
+++ phase2/src/EventPackage/EventUseCases/EventProgramSorter.java
@@ -1,8 +1,9 @@
 package EventPackage.EventUseCases;
 
-import EventPackage.EventEntities.Event;
+import EventPackage.EventEntities.*;
 import UserPackage.UserManager;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -34,10 +35,15 @@ public class EventProgramSorter {
     private ArrayList<String[]> eventsToString(ArrayList<Event> events){
         ArrayList<Event> sortedEvents = new ArrayList(events);
         Collections.sort(sortedEvents);
-
+        // Call generateEventInfo on sortedEvents
+        return new ArrayList<>();
     }
 
-    private String generateExportForEvents(ArrayList<Event> events){
+    private String getDateIdentifier(Date date){
+        return dateFormatter.format(date);
+    }
+
+    private HashMap<String, ArrayList<String[]>> generateEventInfo(ArrayList<Event> events){
         StringBuilder eventsFormatted = new StringBuilder();
 
         // This hashmap is used to group the events that occur on the same day
@@ -45,19 +51,107 @@ public class EventProgramSorter {
         // The value is an array of all events on that day.
         HashMap<String, ArrayList<Event>> eventsForDay = groupEventsByDay(events);
 
-        // Now sort all of the days in the hashmap
+        // Sort days in the hashmap
         ArrayList<Date> eventDaysInOrder = getEventDaysInOrder(eventsForDay);
+
+        // for return
+        // key is dayFormatted string
+        // value is ArrayList of String[] to rep data for events
+        HashMap<String, ArrayList<String[]>> eventInfo = new HashMap<>();
 
         // with this sorted array, we can iterate through the days
         // build Strings representing the data we need for each day and for each event
         for (Date date : eventDaysInOrder){
-            String dateIdentifier = getDateIdentifier(date);
-            ArrayList<Event> eventsOnDay = eventsForDay.get(dateIdentifier);
-            eventsFormatted.append(generateEventsFormattedForDay(eventTemplate, eventsOnDay));
+            String dateIdentifier = dayFormatter.format(date); // ex: Friday November 27
+            ArrayList<Event> eventsOnDay = eventsForDay.get(dateIdentifier); // eventsOnDay for one day only
+            eventInfo.put(dateIdentifier, generateEventInfoForDay(eventsOnDay));
+            //eventsFormatted += String.format(eventTemplate, time, eventName, speakerName.toString(), eventRoom);
         }
 
-        return eventsFormatted.toString();
+        return eventInfo;
+        // returns Hashmap of key: DAY,  value: ArrayList of String[] that rep data for a single event
+        // each String[] has [time, eventName, speakerName, eventRoom]
     }
+
+    private ArrayList<String[]> generateEventInfoForDay(ArrayList<Event> events){
+        ArrayList<String[]> info = new ArrayList<>();
+        for (Event event : events){
+            info.add(generateEventInfoForEvent(event));
+        }
+        return info;
+    }
+
+    private String[] generateEventInfoForEvent(Event event){
+        Date eventDate = event.getEventDate();
+        String time = timeFormatter.format(eventDate);
+        String eventName = event.getEventName();
+        String speakerName = getSpeakerString(event);
+        String eventRoom = Integer.toString(event.getEventRoom());
+        return new String[]{time, eventName, speakerName, eventRoom};
+    }
+
+    private String getSpeakerString(Event event){
+        int eventID = event.getEventId();
+        String speakerName;
+        if (eventManager.isMultiSpeakerEvent(eventID)){
+            ArrayList<Integer> speakers = ((MultiSpeakerEvent)event).getEventSpeakers();
+            StringBuilder namesBuilder = new StringBuilder();
+            for (int speakerId : speakers){
+                String name = userManager.getUserByID(speakerId).getUsername();
+                namesBuilder.append(name);
+                namesBuilder.append(", ");
+            }
+            speakerName = namesBuilder.substring(0, namesBuilder.length()-2);
+        } else if (eventManager.isSingleSpeakerEvent(eventID)) {
+            int speakerId = ((SingleSpeakerEvent)event).getEventSpeaker();
+            speakerName = userManager.getUserByID(speakerId).getUsername();
+        } else { // it's a party
+            speakerName = "";
+        }
+        return speakerName;
+    }
+
+/*    private String generateEventsFormattedForDay(String eventTemplate, ArrayList<Event> events){
+        // format the header for this day
+        Date day = events.get(0).getEventDate();
+        String dateIdentifier = dayFormatter.format(day);
+        String eventsHeaderFormatted = String.format(dateHeaderTemplate, dateIdentifier);
+
+        // now format all the events for this day
+        String eventsFormatted = "";
+        for(int i = 0; i < events.size(); i++){
+            Event event = events.get(i);
+            String eventName = event.getEventName();
+            //int eventID = event.getEventId();
+           *//* if (eventManager.isMultiSpeakerEvent(eventID)){
+                ArrayList<Integer> speakers = ((MultiSpeakerEvent)event).getEventSpeakers();
+            } else if (eventManager.isSingleSpeakerEvent(eventID)) {
+                speakerName = "";
+            } else { // it's a party
+                speakerName = "";
+            }*//*
+
+            ArrayList<Integer> speakerIDs = eventManager.getSpeakerIDs(event);
+            StringBuilder speakerName = new StringBuilder();
+            for (int ID: speakerIDs){
+                speakerName.append(userManager.getUserByID(ID).getUsername());
+            }
+            //= userManager.getUserByID(userID).getUsername();
+            Date eventDate = event.getEventDate();
+            String time = timeFormatter.format(eventDate);
+            String eventRoom = Integer.toString(event.getEventRoom());
+
+            eventsFormatted += String.format(eventTemplate, time, eventName, speakerName.toString(), eventRoom);
+
+            // add a space to separate this event and the next event
+            if(i < events.size() - 1){
+                // eventsFormatted += "<hr class='hr-slim'>";
+                eventsFormatted += "<br>";
+            }
+        }
+        return eventsHeaderFormatted + eventsFormatted;
+    }*/
+
 
     private HashMap<String, ArrayList<Event>> groupEventsByDay(ArrayList<Event> events){
         HashMap<String, ArrayList<Event>> eventsForDay = new HashMap();
@@ -78,12 +172,10 @@ public class EventProgramSorter {
         return eventsForDay;
     }
 
-    private String getDateIdentifier(Date date){
-        return dateFormatter.format(date);
-    }
+
 
     private ArrayList<Date> getEventDaysInOrder(HashMap<String, ArrayList<Event>> eventsForDay){
-        ArrayList<Date> eventDatesInOrder = new ArrayList();
+        ArrayList<Date> eventDatesInOrder = new ArrayList<>();
         // first loop through all the events for each day
         for (HashMap.Entry<String, ArrayList<Event>> eventsOnDay : eventsForDay.entrySet()){
             // then get the date of one event for that day
