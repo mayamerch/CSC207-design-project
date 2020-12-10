5c943c3fc66253340a5b34d3b37032d40037226e
ritvikpuri <ritvikpuri00@gmail.com>
2020-12-10 14:38:59 -0500

    Added SendMessageToAllAttendees, SendMessageToAllSpeakers

---
+++ phase2/src/MessagePackage/MessageGUI/SendMessageToAllSpeakers.java
@@ -0,0 +1,30 @@
+package MessagePackage.MessageGUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SendMessageToAllSpeakers extends JFrame{
+    private JTextArea messageArea;
+    private JButton sendMessagesButton;
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public SendMessageToAllSpeakers(){
+        super();
+        this.setContentPane(getMainPanel());
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.pack();
+
+        sendMessagesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = messageArea.getText();
+                //TODO: send message to all speakers
+            }
+        });
+    }
+}
