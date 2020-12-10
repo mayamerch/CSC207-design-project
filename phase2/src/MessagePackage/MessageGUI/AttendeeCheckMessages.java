3b1dc6f4931a9acd39aca1524bd24acd620ec5b9
ritvikpuri <ritvikpuri00@gmail.com>
2020-12-10 06:09:37 -0500

    Added ViewFriendsForm

--- phase2/src/MessagePackage/MessageGUI/AttendeeCheckMessages.java
+++ phase2/src/MessagePackage/MessageGUI/AttendeeCheckMessages.java
@@ -4,6 +4,8 @@ import GUI.Presenter;
 import UserPackage.UserController;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 public class AttendeeCheckMessages extends JFrame{
@@ -13,18 +15,40 @@ public class AttendeeCheckMessages extends JFrame{
     private JTextField enterID;
     private JButton viewFriends;
 
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
     public AttendeeCheckMessages(UserController loggedInUserContoller) {
         super();
         this.setContentPane(getMainPanel());
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.pack();
 
+        //TODO: make loggedInUser
+
+        viewFriends.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: pass in user to viewFriendsForm
+                ViewFriendsForm viewFriendsForm = new ViewFriendsForm();
+                viewFriendsForm.setContentPane(viewFriendsForm.getMainPanel());
+                viewFriendsForm.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                viewFriendsForm.pack();
+                viewFriendsForm.setVisible(true);
+                setVisible(true);
+            }
+        });
+
+        showMessages.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String userID = enterID.getText();
+                //TODO: get messages from userID and current user and display in textarea
+            }
+        });
+
+
     }
-
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
-
-
 
 }
